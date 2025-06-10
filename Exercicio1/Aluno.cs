8b4194b2894d63b482c442aa680c6904e54dbf17
GraziellaPereira <graziellapereira52@gmail.com>
2025-06-10 12:10:42 -0300

    Adicionar arquivos de projeto.

---
+++ Exercicio1/Aluno.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Exercicio1
+{
+    public class Aluno
+    {
+        public string RA { get; set; }
+        public string Nome { get;  set; }
+        public int Idade { get;  set; }
+        public static List <Aluno> listaAlunos = new List <Aluno> ();
+        private static int proximoRA = 1;
+
+        public static void CadastrarAluno()
+        {
+            Aluno novoAluno = new Aluno();
+            novoAluno.RA = proximoRA.ToString();
+            Console.WriteLine("Digite o nome do aluno: ");
+            novoAluno.Nome = Console.ReadLine();
+            Console.WriteLine("Digite a idade do aluno: ");
+            novoAluno.Idade = int.Parse(Console.ReadLine());
+            listaAlunos.Add(novoAluno);
+            Console.WriteLine($"Aluno cadastrado com o RA {novoAluno.RA}");
+            proximoRA++;
+        }
+        public static void ListarAlunos()
+        {
+            Console.WriteLine("\t Listando todos os alunos...");
+            foreach (var aluno in listaAlunos) {
+                Console.WriteLine($"\n RA: {aluno.RA} \n Nome: {aluno.Nome} \n Idade: {aluno.Idade}");
+            }
+        }
+        public static void AlterarDados()
+        {
+            Console.WriteLine("Digite o RA do aluno que deseja alterar: ");
+            string ra = Console.ReadLine(); 
+
+            if (listaAlunos.Any(a => a.RA == ra)) { }
+        }
+        public void RemoverAluno()
+        {
+
+        }
+    }
+}
---
+++ Exercicio1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t --- BEM VINDO AO GERENCIADOR DE ALUNOS! -- ");
+            bool executando = true;
+
+            while (executando) {
+                Console.WriteLine("\n Digite o que deseja fazer: ");
+                Console.WriteLine("1- Cadastrar um aluno");
+                Console.WriteLine("2- Listar todos os alunos");
+                Console.WriteLine("3- Alterar dados de um aluno");
+                Console.WriteLine("4- Remover um aluno");
+                Console.WriteLine("5- Sair");
+
+                int opcao = int.Parse(Console.ReadLine());
+                switch (opcao)
+                {
+                    case 1:
+                        Aluno.CadastrarAluno();
+                        break;
+
+                    case 2:
+                        Aluno.ListarAlunos();
+                        break;
+                }
+            }
+        }
+    }
+}
