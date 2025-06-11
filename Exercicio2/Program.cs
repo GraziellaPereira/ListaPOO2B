ccfb579f2a4558392821e04febb18cad4bb13dd9
GraziellaPereira <graziellapereira52@gmail.com>
2025-06-10 21:45:58 -0300

    Começando exercicio 2

---
+++ Exercicio2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Exercicio2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t --- BEM VINDO AO SISTEMA DE GERENCIAMENTO DE PRODUTOS! ---");
+            bool executando = true;
+            while (executando)
+            {
+
+            }
+        }
+    }
+}
