567aed9a5a2d493ec14e291198af1f4afacb09ef
Grazi <134076345+GraziellaPereira@users.noreply.github.com>
2025-06-25 16:05:09 -0300

    atualizado exercicio 3

---
+++ Exercicio3/PagamentoPIX.cs
@@ -0,0 +1,20 @@
+﻿using interfacePagamentos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Boleto
+{
+    public class PagamentoPIX : IPagamentos
+    {
+        public void ProcessarPagamento(double valor)
+        {
+            Console.WriteLine($"Pagamento de R${valor:F2} processado via PIX.");
+        }
+        public override string ToString()
+        {
+            return "PIX";
+        }
+
+    }
+}
